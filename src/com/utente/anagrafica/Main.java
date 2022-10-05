63e0938f3fa1dfb4d7611945811d53e062da7d83
Valentina <valelore98@gmail.com>
2022-10-05 16:42:24 +0200

    Created and printed hardcoded variables with user anagraphics

--- src/com/utente/anagrafica/Main.java
+++ src/com/utente/anagrafica/Main.java
@@ -1,5 +1,22 @@
 package com.utente.anagrafica;
 
 public class Main {
-
+	public static void main(String[] args) {
+		String name = "Ajeje", surname = "Brazorf", birthPlace = "PaLeRmO";
+		int age = 30, height = 192;
+		char gender = 'M';
+		boolean hasDriveLicense = false;
+		
+		System.out.println(" _____________________________");
+		System.out.println("|  " + "                         " + "  |");
+		System.out.println("|  " + "Nome: " + name + "                |");
+		System.out.println("|  " + "Cognome: " + surname + "           |");
+		System.out.println("|  " + "Eta': " + age + "                   |");
+		System.out.println("|  " + "Luogo di nascita: " + birthPlace.toLowerCase() + "  |");
+		System.out.println("|  " + "Altezza: " + String.format("%.2f m", (float)height/100) + "            |");
+		System.out.println("|  " + "Sesso: " + gender + "                   |");
+		System.out.println("|  " + "Patente: " + (hasDriveLicense ? "SI" : "NO") + "                |");
+		System.out.println("|  " + "                         " + "  |");
+		System.out.println("|_____________________________|");
+	}
 }
